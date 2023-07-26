a02bc6eef6a1a0fb49653cb12beeb2f75229b5da
Zekiloni <zekirija17@gmail.com>
2023-07-26 21:51:42 +0200

    formatted tour schema and controller

--- motorradtouren-server/src/tour/tour.controller.ts
+++ motorradtouren-server/src/tour/tour.controller.ts
@@ -1,27 +1,21 @@
-import {
-   Controller,
-   UseInterceptors,
-   Body,
-   Post,
-   UploadedFiles,
-} from '@nestjs/common';
+import { Controller, UseInterceptors, Body, Post, UploadedFiles } from '@nestjs/common';
 import { FileFieldsInterceptor } from '@nestjs/platform-express';
 import { CreateTourDto } from './dto/create-tour.dto';
 
 @Controller('tour')
 export class TourController {
-   @Post()
-   @UseInterceptors(
-      FileFieldsInterceptor([
-         { name: 'tourGpx', maxCount: 1 },
-         { name: 'tourImage', maxCount: 1 },
-      ]),
-   )
-   create(
-      @Body() createTourDto: CreateTourDto,
-      @UploadedFiles()
-      files: { tourGpx: Express.Multer.File; tourImage: Express.Multer.File },
-   ) {
-      console.log(files);
-   }
+  @Post()
+  @UseInterceptors(
+    FileFieldsInterceptor([
+      { name: 'tourGpx', maxCount: 1 },
+      { name: 'tourImage', maxCount: 1 },
+    ]),
+  )
+  create(
+    @Body() createTourDto: CreateTourDto,
+    @UploadedFiles()
+    files: { tourGpx: Express.Multer.File; tourImage: Express.Multer.File },
+  ) {
+    console.log(files);
+  }
 }
--- motorradtouren-server/src/tour/tour.schema.ts
+++ motorradtouren-server/src/tour/tour.schema.ts
@@ -3,25 +3,25 @@ import { Document, Types } from 'mongoose';
 import { User } from 'src/user/user.schema';
 
 @Schema({
-  timestamps: {
-    createdAt: 'createdAt'
-  },
+   timestamps: {
+      createdAt: 'createdAt',
+   },
 })
 export class Tour extends Document {
-  @Prop()
-  title: string;
+   @Prop()
+   title: string;
 
-  @Prop()
-  description: string;
+   @Prop()
+   description: string;
 
-  @Prop({ type: Types.ObjectId, ref: User.name })
-  author: User;
+   @Prop({ type: Types.ObjectId, ref: User.name })
+   author: User;
 
-  @Prop()
-  imageUrl: string;
+   @Prop()
+   imageUrl: string;
 
-  @Prop()
-  gpxUrl: string;
+   @Prop()
+   gpxUrl: string;
 }
 
 export const TourSchema = SchemaFactory.createForClass(Tour);
