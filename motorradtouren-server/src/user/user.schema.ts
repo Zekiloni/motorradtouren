a2009c1561360c2abc45d9e5c282d4e4c42d5158
Zekiloni <zekirija17@gmail.com>
2023-07-26 22:19:25 +0200

    format

--- motorradtouren-server/src/user/user.schema.ts
+++ motorradtouren-server/src/user/user.schema.ts
@@ -4,33 +4,36 @@ import bcrypt from 'bcryptjs';
 
 @Schema()
 export class User extends Document {
-  @Prop({ unique: true })
-  username: string;
+   @Prop({ unique: true })
+   username: string;
 
-  @Prop()
-  password: string;
+   @Prop()
+   password: string;
 
-  @Prop({ default: false })
-  isAdministrator: boolean;
+   @Prop({ default: false })
+   isAdministrator: boolean;
 
-  @Prop({ default: Date.now })
-  createdAt: Date;
+   @Prop({ default: null })
+   ride: string | null;
+
+   @Prop({ default: Date.now })
+   createdAt: Date;
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
 
 UserSchema.pre<User>('save', async function (next) {
-  if (this.isModified('password') || this.isNew) {
-    try {
-      const hashedPassword = await bcrypt.hash(
-        this.password,
-        process.env.PASSWORD_SALT_ROUNDS || 15,
-      );
-      
-      this.password = hashedPassword;
-    } catch (error) {
-      return next(error);
-    }
-  }
-  next();
+   if (this.isModified('password') || this.isNew) {
+      try {
+         const hashedPassword = await bcrypt.hash(
+            this.password,
+            process.env.PASSWORD_SALT_ROUNDS || 15,
+         );
+
+         this.password = hashedPassword;
+      } catch (error) {
+         return next(error);
+      }
+   }
+   next();
 });
